205e1466605df503fcd854559e959a8fb9f2c42b
Silvio Henrique <37641559+svhenrique@users.noreply.github.com>
2021-04-26 22:45:25 -0300

    insertion consertado

--- ED/algoritmos de ordenacao/insertion_sort.py
+++ ED/algoritmos de ordenacao/insertion_sort.py
@@ -1,4 +1,4 @@
-'''def insertion_sort(lista):
+def insertion_sort(lista):
 	for i in range(1,len(lista)):
 		j = i
 		while j>0 and lista[j]<lista[j-1]:
@@ -6,13 +6,5 @@
 			j-=1
 	return lista
 print(insertion_sort([1,2,3,4,5,6,7]))
-'''
-def selection(lista):
-        for i in range(len(lista)):
-                menor = i
-                for j in range(i, len(lista)):
-                        if lista[menor]>lista[j]:
-                                menor = j
-                lista[menor], lista[i] = lista[i], lista[menor]
-        return lista
-print(selection([2,8,4,6,1,9]))
+
+
